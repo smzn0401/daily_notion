33e1963339271cd0c7bb881e148098d082bf49cb
smzn0401 <yoh.enby@gmail.com>
2025-12-12 11:25:58 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style= padding: 16 >
+      <h1>Daily Timeline</h1>
+      <p><a href="/daily/today">/daily/today を開く</a></p>
+    </main>
+  );
+}
